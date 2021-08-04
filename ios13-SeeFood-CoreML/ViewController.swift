1b143d6cc21cc190432d287dffb3005ae87894a9
Han-Saem Park <samiparken@gmail.com>
2021-08-04 15:01:48 +0200

    init

---
+++ ios13-SeeFood-CoreML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios13-SeeFood-CoreML
+//
+//  Created by Sam on 2021-08-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
